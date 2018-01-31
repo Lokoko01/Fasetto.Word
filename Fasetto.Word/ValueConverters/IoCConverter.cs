43a896dccc460827434ddfd8c16b3d600f24869b
Martin <martin.rascle@gimel.fr>
2018-01-31 14:24:22 +0100

    Ajoutez des fichiers projet.

---
+++ Fasetto.Word/ValueConverters/IoCConverter.cs
@@ -0,0 +1,32 @@
+﻿using Fasetto.Word.Core;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Converts a string name to a service pulled from the IoC container
+    /// </summary>
+    public class IoCConverter : BaseValueConverter<IoCConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Find the appropriate page
+            switch ((string)value)
+            {
+                case nameof(ApplicationViewModel):
+                    return IoC.Application;
+
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
