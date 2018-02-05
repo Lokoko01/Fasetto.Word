c0466b2aa630219387358a2602015965703246e1
Martin <martin.rascle@gimel.fr>
2018-02-05 10:01:01 +0100

    Add chat message search

--- Fasetto.Word.Core/ViewModel/Chat/ChatMessage/ChatMessageListViewModel.cs
+++ Fasetto.Word.Core/ViewModel/Chat/ChatMessage/ChatMessageListViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace Fasetto.Word.Core
@@ -10,12 +11,63 @@ namespace Fasetto.Word.Core
     /// </summary>
     public class ChatMessageListViewModel : BaseViewModel
     {
-        #region Public Properties
+        #region Protected Members
+        
+        /// <summary>
+        /// The last searched text in this list
+        /// </summary>
+        protected string mLastSearchText;
+
+        /// <summary>
+        /// The text to search for in the search command
+        /// </summary>
+        protected string mSearchText;
 
         /// <summary>
         /// The chat thread items for the list
         /// </summary>
-        public ObservableCollection<ChatMessageListItemViewModel> Items { get; set; }
+        protected ObservableCollection<ChatMessageListItemViewModel> mItems;
+
+        /// <summary>
+        /// A flag indicating if the search dialog is open
+        /// </summary>
+        protected bool mSearchIsOpen;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// The chat thread items for the list
+        /// NOTE: Do not call Items.Add to add messages to this list
+        /// as it will make the FilteredItems out of sync
+        /// </summary>
+        public ObservableCollection<ChatMessageListItemViewModel> Items
+        {
+            get => mItems;
+            set
+            {
+                // Make sure list has changed
+                if (mItems == value)
+                    return;
+
+                // Update value
+                mItems = value;
+
+                // Update filtered list to match
+                FilteredItems = new ObservableCollection<ChatMessageListItemViewModel>(mItems);
+            }
+        }
+
+        /// <summary>
+        /// The chat thread items for the list that include any search filtering
+        /// </summary>
+        public ObservableCollection<ChatMessageListItemViewModel> FilteredItems { get; set; }
+
+        /// <summary>
+        /// The title of this chat list
+        /// </summary>
+        public string DisplayTitle { get; set; }
 
         /// <summary>
         /// True to show the attachment menu, false to hide it
@@ -23,7 +75,7 @@ namespace Fasetto.Word.Core
         public bool AttachmentMenuVisible { get; set; }
 
         /// <summary>
-        /// True if any popup menus are visible
+        /// True if any pop-up menus are visible
         /// </summary>
         public bool AnyPopupVisible => AttachmentMenuVisible;
 
@@ -36,6 +88,50 @@ namespace Fasetto.Word.Core
         /// The text for the current message being written
         /// </summary>
         public string PendingMessageText { get; set; }
+
+        /// <summary>
+        /// The text to search for when we do a search
+        /// </summary>
+        public string SearchText
+        {
+            get => mSearchText;
+            set
+            {
+                // Check value is different
+                if (mSearchText == value)
+                    return;
+
+                // Update value
+                mSearchText = value;
+
+                // If the search is empty
+                if (string.IsNullOrEmpty(SearchText))
+                    // Search to restor messages
+                    Search();
+            }
+        }
+
+        /// <summary>
+        /// Flag indicating if the search dialog is open
+        /// </summary>
+        public bool SearchIsOpen
+        {
+            get => mSearchIsOpen;
+            set
+            {
+                // Check value is different
+                if (mSearchIsOpen == value)
+                    return;
+
+                // Update value
+                mSearchIsOpen = value;
+
+                // If dialog closes...
+                if (!mSearchIsOpen)
+                    // Clear search text
+                    SearchText = string.Empty;
+            }
+        }
 
         #endregion
 
@@ -56,6 +152,26 @@ namespace Fasetto.Word.Core
         /// </summary>
         public ICommand SendCommand { get; set; }
 
+        /// <summary>
+        /// The command for when the user wants to search
+        /// </summary>
+        public ICommand SearchCommand { get; set; }
+
+        /// <summary>
+        /// The command for when the user wants to open the search the dialog
+        /// </summary>
+        public ICommand OpenSearchCommand { get; set; }
+
+        /// <summary>
+        /// The command for when the user wants to close the search dialog
+        /// </summary>
+        public ICommand CloseSearchCommand { get; set; }
+
+        /// <summary>
+        /// The command for when the user wants to clear the search dialog
+        /// </summary>
+        public ICommand ClearSearchCommand { get; set; }
+
         #endregion
 
         #region Constructor
@@ -69,6 +185,11 @@ namespace Fasetto.Word.Core
             AttachmentButtonCommand = new RelayCommand(AttachmentButton);
             PopupClickawayCommand = new RelayCommand(PopupClickaway);
             SendCommand = new RelayCommand(Send);
+
+            SearchCommand = new RelayCommand(Search);
+            OpenSearchCommand = new RelayCommand(OpenSearch);
+            CloseSearchCommand = new RelayCommand(CloseSearch);
+            ClearSearchCommand = new RelayCommand(ClearSearch);
 
             // Make a default menu
             AttachmentMenu = new ChatAttachmentPopupMenuViewModel();
@@ -88,7 +209,7 @@ namespace Fasetto.Word.Core
         }
 
         /// <summary>
-        /// When the popup clickaway area is clicked hide any popups
+        /// When the pop-up clickaway area is clicked hide any popups
         /// </summary>
         public void PopupClickaway()
         {
@@ -101,13 +222,16 @@ namespace Fasetto.Word.Core
         /// </summary>
         public void Send()
         {
+            // Ensure list is not null
             if (Items == null)
                 Items = new ObservableCollection<ChatMessageListItemViewModel>();
+            if (FilteredItems == null)
+                FilteredItems = new ObservableCollection<ChatMessageListItemViewModel>();
 
             if (!string.IsNullOrWhiteSpace(PendingMessageText))
             {
                 // Fake send a new message
-                Items.Add(new ChatMessageListItemViewModel
+                var message = new ChatMessageListItemViewModel
                 {
                     Initials = "LM",
                     Message = PendingMessageText,
@@ -115,12 +239,71 @@ namespace Fasetto.Word.Core
                     SentByMe = true,
                     SenderName = "Luke Malpass",
                     NewItem = true
-                });
+                };
+
+                // Add message to both list
+                Items.Add(message);
+                FilteredItems.Add(message);
             }
 
             // Clear the pending message text
             PendingMessageText = string.Empty;
         }
+
+        /// <summary>
+        ///  Seaches the current message list and filters the view
+        /// </summary>
+        public void Search()
+        {
+            // Make sure we don't re-search the same text
+            if ((string.IsNullOrEmpty(mLastSearchText) && string.IsNullOrEmpty(SearchText)) ||
+                    string.Equals(mLastSearchText, SearchText))
+                return;
+
+            // If we have no search text, or no items
+            if (string.IsNullOrEmpty(SearchText) || Items == null || Items.Count <= 0)
+            {
+                // Make filtered list the same
+                FilteredItems = new ObservableCollection<ChatMessageListItemViewModel>(Items);
+
+                // Set last search
+                mLastSearchText = SearchText;
+
+                return;
+            }
+
+            // Find all items that contain the given text
+            // TODO: Make more efficient search
+            FilteredItems = new ObservableCollection<ChatMessageListItemViewModel>(
+                Items.Where(Items => Items.Message.ToLower().Contains(SearchText)));
+
+            // Set last search
+            mLastSearchText = SearchText;
+        }
+
+        /// <summary>
+        /// Clears the search text
+        /// </summary>
+        public void ClearSearch()
+        {
+            // If there is some search text...
+            if (!string.IsNullOrEmpty(SearchText))
+                // CLear the text
+                SearchText = string.Empty;
+            else
+                // Close search dialog
+                SearchIsOpen = false;
+        }
+
+        /// <summary>
+        /// Opens the search dialog
+        /// </summary>
+        public void OpenSearch() => SearchIsOpen = true;
+
+        /// <summary>
+        /// Closes the search dialog
+        /// </summary>
+        public void CloseSearch() => SearchIsOpen = false;
 
         #endregion
     }
